5db44bc272fab427c6ea5dd5ae23d843fc479981
jean00 <jeanlouis433@gmail.com>
2022-03-22 20:56:18 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./Components/NavBar";
+import About from "./Components/About";
+import Skills from "./Components/Skills";
+import Projects from "./Components/Projects";
+import Contacts from "./Components/Contacts";
+import "./Style/style.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <NavBar></NavBar>
       </header>
+      <main>
+        <About></About>
+        <Projects></Projects>
+        <Skills></Skills>
+        <Contacts></Contacts>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <header>
+      <div> Jean </div>
+    </header>
+  );
+};
+
+export default NavBar;
